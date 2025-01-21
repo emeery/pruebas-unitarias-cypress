7b21c3bd3b1921fc5d5dfb8795e7c8f196df2656
Carlos Solis <carlos.solis.d@gmail.com>
2025-01-21 02:38:01 -0300

    03_02

--- cypress/e2e/03/03_01_input_texto.cy.js
+++ cypress/e2e/03/03_01_input_texto.cy.js
@@ -9,4 +9,10 @@ describe('Register Form', () => {
         cy.get('div.text-danger').should('contain', 'El nombre debe tener al menos 3 caracteres');
     });
 
+    it('no debe mostrar error si el nombre es mayor de 4 caracteres', () => {
+        cy.get('input[name="name"]').type('John');
+        cy.get('form').submit();
+        cy.get('form :nth-child(1) > .text-danger').should('not.exist');
+    });
+
 });
